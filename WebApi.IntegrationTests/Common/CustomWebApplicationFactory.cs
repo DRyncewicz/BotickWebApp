c50cd3e2fb7285efb78a8fa178f45bdeb0747810
Dominik Ryryry <ereence@gmail.com>
2024-01-27 18:34:19 +0100

    tests: Add test enviroment for integration tests

---
+++ WebApi.IntegrationTests/Common/CustomWebApplicationFactory.cs
@@ -0,0 +1,55 @@
+﻿using BotickAPI.Application.Common.Interfaces;
+using BotickAPI.Persistence.Context;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace WebApi.IntegrationTests.Common
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            try
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                    services.AddDbContext<BotickDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("InMemoryDatabase");
+                        options.UseInternalServiceProvider(serviceProvider);
+                    });
+                    services.AddScoped<IBotickDbContext>(provider => provider.GetService<BotickDbContext>());
+
+                    var sp = services.BuildServiceProvider();
+
+                    using var scope = sp.CreateScope();
+                    var scopedService = scope.ServiceProvider;
+                    var context = scopedService.GetRequiredService<BotickDbContext>();
+                    var logger = scopedService.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    context.Database.EnsureCreated();
+
+                    try
+                    {
+                        Utilities.InitializeDbForTests(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the" +
+                            $"databgase with test messages. Error: {ex.Message}");
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+
+            }
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
